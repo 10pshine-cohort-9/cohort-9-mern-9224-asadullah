ea727aac90c51a68193992c9e186d01695ea8545
Https-Asad <asadullahcodes@gmail.com>
2026-08-04 00:00:08 +0500

    setup frontend and UI

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,9 +5,15 @@ const noteRoutes = require('./routes/note.routes')
 const logger = require('./utils/logger');
 const errorHandler = require('./middleware/error.middleware')
 const httpLogger = require('./middleware/httpLogger.middleware')
+const cors = require('cors')
 
 const app = express()
 
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  })
+)
 
 
 app.use(httpLogger)
